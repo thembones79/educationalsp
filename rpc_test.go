b738527cfd284b37440b44b61062e91f3afd762a
thembones79 <michal.szumnarski@gmail.con>
2024-04-06 11:22:34 +0200

    started work on decoding

--- rpc_test.go
+++ rpc_test.go
@@ -16,3 +16,15 @@ func TestEncode(t *testing.T) {
 		t.Fatalf("Expected: %s, Actual: %s", expected, actual)
 	}
 }
+
+func TestDecode(t *testing.T) {
+	incomingMessage := "Content-Length: 16\r\n\r\n{\"Testing\":true}"
+	contentLength, err := rpc.DecodeMessage([]byte(incomingMessage))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if contentLength != 16 {
+		t.Fatalf("Expected: 16, Got: %d", contentLength)
+	}
+}
