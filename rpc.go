5360671013a07ea76cfb33a0b3265ee893029a3e
thembones79 <michal.szumnarski@gmail.con>
2024-04-06 18:53:06 +0200

    continue work on decoding

--- rpc.go
+++ rpc.go
@@ -17,19 +17,25 @@ func EncodeMessage(msg any) string {
 	return fmt.Sprintf("Content-Length: %d\r\n\r\n%s", len(content), content)
 }
 
-func DecodeMessage(msg []byte) (int, error) {
+type BaseMessage struct {
+	Method string `json: "method"`
+}
+
+func DecodeMessage(msg []byte) (string, []byte, error) {
 	header, content, found := bytes.Cut(msg, []byte{'\r', '\n', '\r', '\n'})
 	if !found {
-		return 0, errors.New("Did not found separator")
+		return "", nil, errors.New("Did not found separator")
 	}
 	contentLengthBytes := header[len("Content-Length: "):]
 	contentLength, err := strconv.Atoi(string(contentLengthBytes))
 	if err != nil {
-		return 0, err
+		return "", nil, err
 	}
 
-    //TODO
-    _ = content
+	var baseMessage BaseMessage
+	if err := json.Unmarshal(content[:contentLength], &baseMessage); err != nil {
+		return "", nil, err
+	}
 
-	return contentLength, nil
+	return baseMessage.Method, content, nil
 }
